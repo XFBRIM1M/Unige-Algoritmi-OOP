0f8d8cb9302775dcaa70de31860c658101add3ba
Gabriele Ventrice <5325135@studenti.unige.it>
2023-09-28 11:42:44 +0200

    Added some functions

--- Algoritmi OPP/Functions/funzioni.cpp
+++ Algoritmi OPP/Functions/funzioni.cpp
@@ -16,3 +16,42 @@ void Stampa(float *vt, int d)
     cout<<vt[i]<<" ";
     cout<<endl;
 }
+
+
+float eulerianInteger(float (*f)(float), float a, float b, int N)
+{
+    float h = (b-a) / (N-1);
+    float x, Q = 0;
+    for (int i = 0 ; i<= N-2; i++)
+    {
+        x = a + i * h;
+        Q += (*f) (x);
+    }
+}
+
+void byteDimension()
+{
+    int a = 2;
+    
+    cout << "Dimensione intero in byte: "<<sizeof(a) << endl;
+    cout << a / 3 << endl;
+    
+    a = (int) pow(a, 31)-1;
+    cout << a << endl;
+    
+    cout << a + 1 << endl;
+}
+
+void roundError()
+{
+    float x = 0.5, tmp=0;
+    for (int i = 0; i < 1e5; i++)
+        tmp += x;
+    cout << tmp << endl;
+    x = 0.1;
+    tmp = 0;
+    for (int i = 0; i < 1e5; i++)
+        tmp += x;
+    cout << tmp << endl;
+}
+
--- Algoritmi OPP/Headers/funzioni.h
+++ Algoritmi OPP/Headers/funzioni.h
@@ -1,6 +1,12 @@
 ﻿#ifndef FUNZIONI_H
 #define FUNZIONI_H
 
-void Leggi(float *, int);
-void Stampa(float *, int);
+void Leggi(float *, int);//Archetype of a Input Function.
+void Stampa(float *, int);//Archetype of Output Function
+void byteDimension(); //Archetype register of total number of byte
+void roundError();//Archetype round error
+
+float eulerianInteger(float(*f)(float), float a, float b, int N);//Integer resolution by the eulerian theory
+
+
 #endif
